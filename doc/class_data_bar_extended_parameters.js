4ea780658effa8d16e640ecf4754e6bf846cf645
Aleksander Grinin <Aleksander.Grinin@aspose.com>
2020-12-31 13:10:19 +0200

    Updated API doc

---
+++ doc/class_data_bar_extended_parameters.js
@@ -0,0 +1,7 @@
+var class_data_bar_extended_parameters =
+[
+    [ "equals", "class_data_bar_extended_parameters.html#af940b98888d0fc2a59c9a4dd85b0fb32", null ],
+    [ "hashcode", "class_data_bar_extended_parameters.html#a49fb42c7665b2d0b995961cadd9a8188", null ],
+    [ "is2DCompositeComponent", "class_data_bar_extended_parameters.html#a9b841d5a7be697a093cc97353abdea54", null ],
+    [ "toString", "class_data_bar_extended_parameters.html#a5558c5d549f41597377fa1ea8a1cefa3", null ]
+];
---
+++ doc/class_enable_checksum.js
@@ -0,0 +1,6 @@
+var class_enable_checksum =
+[
+    [ "DEFAULT", "class_enable_checksum.html#af3ce65309508e2623864deb6996aa88d", null ],
+    [ "NO", "class_enable_checksum.html#ae19911b78bacfc8821b24d36e1f91241", null ],
+    [ "YES", "class_enable_checksum.html#aa8f02f92747fd7830388a4dc09b78b96", null ]
+];
