330287329ec6424ac9a3f80d23eb28167225837c
Aleksander <alex.grinnin@gmail.com>
2025-08-16 14:24:14 +0300

    BARCODEPHP-983 Implement running tests on both php and java sources in folder packagist to enable cross-language testing between php and java for streamlined debugging

--- test/assist/loader.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Centralized loader for all assist tools
-//include_once __DIR__ . '/TestsAssist.php';
-//include_once __DIR__ . '/Assert.php';
-//include_once __DIR__ . '/TestsLauncher.php';
-//include_once __DIR__ . '/TestHelper.php';
--- test/php/building/run_generator_tests.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace building;
-
-include_once('./BarcodeGeneratorTests.php');
-$barcodeGeneratorTests = new BarcodeGeneratorTests();
-$barcodeGeneratorTests->testGenerateBarcodeImage();
-
-?>
