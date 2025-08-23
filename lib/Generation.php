6e45e110d8494592a90325c6c23582e65c2c026a
Aleksander <alex.grinnin@gmail.com>
2025-08-23 16:54:18 +0300

    BARCODEPHP-1001 Data type of enum BorderDashStyle set to string but expected int

--- lib/Generation.php
+++ lib/Generation.php
@@ -8075,24 +8075,24 @@ class BorderDashStyle
     /**
      * Specifies a solid line.
      */
-    const  SOLID = "0"; //DashStyle.Solid
+    const  SOLID = 0; //DashStyle.Solid
     /**
      * Specifies a line consisting of dashes.
      */
-    const   DASH = "1"; // DashStyle.Dash
+    const   DASH = 1; // DashStyle.Dash
     /**
      * Specifies a line consisting of dots.
      */
-    const  DOT = "2"; //(DashStyle.Dot
+    const  DOT = 2; //(DashStyle.Dot
 
     /**
      * Specifies a line consisting of a repeating pattern of dash-dot.
      */
-    const  DASH_DOT = "3"; //DashStyle.DashDot
+    const  DASH_DOT = 3; //DashStyle.DashDot
     /**
      * Specifies a line consisting of a repeating pattern of dash-dot-dot.
      */
-    const  DASH_DOT_DOT = "4"; //DashStyle.DashDotDot
+    const  DASH_DOT_DOT = 4; //DashStyle.DashDotDot
 }
 
 /**
