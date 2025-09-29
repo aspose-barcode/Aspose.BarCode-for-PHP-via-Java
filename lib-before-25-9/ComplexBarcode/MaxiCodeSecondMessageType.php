ae32b319cc3ad52425e4775ba0aed51c30504f74
Aleksander Grinin <Aleksander.Grinin@aspose.com>
2025-09-29 18:33:34 +0300

    Removed assistant folder

--- lib-before-25-9/ComplexBarcode/MaxiCodeSecondMessageType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Aspose\Barcode\ComplexBarcode;
-
-class MaxiCodeSecondMessageType
-{
-    const MAXI_CODE_STANDART_SECOND_MESSAGE = 0;
-    const MAXI_CODE_STRUCTURED_SECOND_MESSAGE = 1;
-}
--- lib-before-25-9/Generation/Base64CodeTextType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Aspose\Barcode\Generation;
-
-class Base64CodeTextType
-{
-    const STRING = 0;
-    const BYTE = 1;
-}
